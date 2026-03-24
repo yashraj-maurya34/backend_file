0911ba230e54f7c1409df648774d9de4b26d59e7
yash <yasahrajkumar34@gmail.com>
2026-03-24 17:01:54 +0530

    login_add

--- index.mjs
+++ index.mjs
@@ -5,7 +5,18 @@ const port =5000
 server.get("/",(req,res)=> {
     res.json({name:"apple"})
 
+
 })
+
+server.use(express.json())
+
+server.post("/login",(req,res)=> {
+    console.log(req.body)
+    res.status(200).json({name:"success"})
+
+})
+
+
 
 server.listen(port,()=>{
     console.log(`server started on ${port}`)
